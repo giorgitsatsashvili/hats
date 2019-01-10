d5e9ce3b6ae50068889246b55cdd43895333cd6a
WannabeRockStar <g.gagloshvili@gmail.com>
2019-01-10 18:14:59 +0400

    category page

---
+++ resources/views/pages/cat.blade.php
@@ -0,0 +1,107 @@
+@extends("layouts/app")
+@section("content")
+	<div class="extra-margin-div">
+		<div class="wrapper">
+			
+			<h1 class="cat-page-title">Category Name</h1>
+			
+			<div class="cat-box">
+				<a href="#">
+					<img src="images/borsalino_medium(1).jpg" alt="image">
+				</a>
+				<h4 class="cat-box-title">
+					<a href="#">Borsalino Medium Hat</a>
+				</h4>
+				<div class="cat-box-icons">
+					<a href="#">
+						<i class="fas fa-shopping-cart"></i>
+					</a>
+					<a href="#">
+						<i class="fas fa-heart"></i>
+					</a>
+				</div>
+			</div>
+			<div class="cat-box">
+				<a href="#">
+					<img src="images/borsalino_medium(1).jpg" alt="image">
+				</a>
+				<h4 class="cat-box-title">
+					<a href="#">Borsalino Medium Hat</a>
+				</h4>
+				<div class="cat-box-icons">
+					<a href="#">
+						<i class="fas fa-shopping-cart"></i>
+					</a>
+					<a href="#">
+						<i class="fas fa-heart"></i>
+					</a>
+				</div>
+			</div>
+			<div class="cat-box">
+				<a href="#">
+					<img src="images/borsalino_medium(1).jpg" alt="image">
+				</a>
+				<h4 class="cat-box-title">
+					<a href="#">Borsalino Medium Hat</a>
+				</h4>
+				<div class="cat-box-icons">
+					<a href="#">
+						<i class="fas fa-shopping-cart"></i>
+					</a>
+					<a href="#">
+						<i class="fas fa-heart"></i>
+					</a>
+				</div>
+			</div>
+			<div class="cat-box">
+				<a href="#">
+					<img src="images/borsalino_medium(1).jpg" alt="image">
+				</a>
+				<h4 class="cat-box-title">
+					<a href="#">Borsalino Medium Hat</a>
+				</h4>
+				<div class="cat-box-icons">
+					<a href="#">
+						<i class="fas fa-shopping-cart"></i>
+					</a>
+					<a href="#">
+						<i class="fas fa-heart"></i>
+					</a>
+				</div>
+			</div>
+			<div class="cat-box">
+				<a href="#">
+					<img src="images/borsalino_medium(1).jpg" alt="image">
+				</a>
+				<h4 class="cat-box-title">
+					<a href="#">Borsalino Medium Hat</a>
+				</h4>
+				<div class="cat-box-icons">
+					<a href="#">
+						<i class="fas fa-shopping-cart"></i>
+					</a>
+					<a href="#">
+						<i class="fas fa-heart"></i>
+					</a>
+				</div>
+			</div>
+			<div class="cat-box">
+				<a href="#">
+					<img src="images/borsalino_medium(1).jpg" alt="image">
+				</a>
+				<h4 class="cat-box-title">
+					<a href="#">Borsalino Medium Hat</a>
+				</h4>
+				<div class="cat-box-icons">
+					<a href="#">
+						<i class="fas fa-shopping-cart"></i>
+					</a>
+					<a href="#">
+						<i class="fas fa-heart"></i>
+					</a>
+				</div>
+			</div>
+
+		</div>
+	</div>
+@endsection
